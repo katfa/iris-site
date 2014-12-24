2646ce122f64d07ea3f82da08a80ec4ffefcc7f9
katfa <kat@finn.no>
2014-12-24 17:20:21 +0100

    pics

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,22 +1,31 @@
 (function () {
     window.addEventListener("DOMContentLoaded", addNavListeners);
 
+    var homeContent = document.getElementById('home-content');
     var currentActive;
     var purple = '#A204E0';
     var darkGrey = '#383838';
 
-    function removeActive(el) {
+    function removeActive (el) {
         el.classList.remove('active');
         el.style.borderBottom = "2px solid " + darkGrey;
         document.querySelector(currentActive.hash).classList.add('hidden');
         currentActive = null;
     }
 
-    function addActive(el) {
+    function addActive (el) {
         el.classList.add('active');
         el.style.borderBottom = "2px solid " + purple;
         currentActive = el;
         document.querySelector(currentActive.hash).classList.remove('hidden');
+    }
+
+    function showHomeContent () {
+        homeContent.classList.remove('hidden');
+    }
+
+    function hideHomeContent () {
+        homeContent.classList.add('hidden');
     }
 
     function addNavListeners() {
@@ -31,6 +40,7 @@
                 } else {
                     addActive(a);
                 }
+                hideHomeContent();
             });
         });
     }
